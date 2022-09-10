c549f4bc0be07ca1447fe5765bd29de441d6ff05
georgewaraw <georgewaraw@gmail.com>
2022-09-10 01:13:10 -0400

    Fix gun SFX on touch

--- src/Gun.jsx
+++ src/Gun.jsx
@@ -12,20 +12,59 @@ export function Gun() {
 
 	useEffect( function () {
 
-		function pointerDownCallback( e ) {
+		let timeThen = 0;
+		let xStart = 0;
+		let yStart = 0;
+
+		function touchStartCallback( e ) {
 
 			e.preventDefault();
 
-			if( audioShoot.current ) {
+			timeThen = Date.now();
 
-				if( audioShoot.current.isPlaying ) audioShoot.current.stop();
-				audioShoot.current.play();
+			xStart = e.changedTouches[ 0 ].clientX / window.innerWidth * 2 - 1;
+			yStart = e.changedTouches[ 0 ].clientY / window.innerHeight * -2 + 1;
+		}
+		function pointerUpCallback( e ) {
+
+			e.preventDefault();
+
+			if(
+				!timeThen ||
+				Date.now() - timeThen < 200
+			) {
+
+				const xEnd = e.clientX / window.innerWidth * 2 - 1;
+				const yEnd = e.clientY / window.innerHeight * -2 + 1;
+
+				if(
+					(
+						!xStart &&
+						!yStart
+					) ||
+					(
+						Math.abs( xStart - xEnd ) < 0.2 &&
+						Math.abs( yStart - yEnd ) < 0.2
+					)
+				) {
+
+					if( audioShoot.current ) {
+
+						if( audioShoot.current.isPlaying ) audioShoot.current.stop();
+						audioShoot.current.play();
+					}
+				}
 			}
 		}
 
-		window.addEventListener( "pointerdown", pointerDownCallback );
+		window.addEventListener( "touchstart", touchStartCallback );
+		window.addEventListener( "pointerup", pointerUpCallback );
 
-		return () => window.removeEventListener( "pointerdown", pointerDownCallback );
+		return function () {
+
+			window.removeEventListener( "touchstart", touchStartCallback );
+			window.removeEventListener( "pointerup", pointerUpCallback );
+		}
 	}, [] );
 
 	return (
